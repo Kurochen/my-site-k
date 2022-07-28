c060b4d247545aa976869c085d877d01b11194db
Alexandr K <kurochen@ya.ru>
2022-07-28 09:57:07 +0700

    Boilerplate

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,12 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-vanilla-extract',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 }
 
 export default config
