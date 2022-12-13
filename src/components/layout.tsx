09799d04f4d0a78498ab73edb2c21c9eeedf2ecf
Alexandr K <kurochen@ya.ru>
2022-12-13 14:45:55 +0700

    Start go next version 5

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,28 +1,26 @@
-import * as React from "react"
+import * as React from "react";
 // import Header from "./header"
-import Footer from "./footer"
-import Head from "./head"
-import "../styles.css"
-import { Flex } from "./ui"
+import Footer from "./footer";
+import Head from "./head";
+import "../styles.css";
+import { Flex } from "./ui";
 
 interface LayoutProps {
-    children: React.ReactNode
-    title: string
-    description?: string
-    image?: { id: string; url: string }
+  children: React.ReactNode;
+  title: string;
+  description?: string;
+  image?: { id: string; url: string };
 }
 
 const Layout = (props: LayoutProps) => {
-    return (
-        <Flex variant="column">
-            <Head {...props} />
-            {/* <Header /> */}
-            <React.Fragment>
-                {props.children}
-            </React.Fragment>
-            <Footer />
-        </Flex>
-    )
-}
+  return (
+    <Flex variant="column">
+      <Head {...props} />
+      {/* <Header /> */}
+      <React.Fragment>{props.children}</React.Fragment>
+      <Footer />
+    </Flex>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
-import * as React from "react"
-import FeatureList from "../components/feature-list"
-import Hero from "../components/hero"
-import Layout from "../components/layout"
-import LogoList from "../components/logo-list"
+import * as React from "react";
+import FeatureList from "../components/feature-list";
+import Hero from "../components/hero";
+import Layout from "../components/layout";
+import LogoList from "../components/logo-list";
 
 function IndexPage() {
   return (
     <Layout title="Главная">
       <Hero />
-      <LogoList />
       <FeatureList />
+      <LogoList />
     </Layout>
-
-  )
+  );
 }
 
-export default IndexPage
+export default IndexPage;
