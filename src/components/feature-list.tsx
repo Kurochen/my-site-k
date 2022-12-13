393a0d8df9a4ace1c9870ca1c31adaed82eea104
Alexandr K <kurochen@ya.ru>
2022-12-13 15:57:45 +0700

    Updated and edited

--- src/components/feature-list.tsx
+++ src/components/feature-list.tsx
@@ -1,88 +1,89 @@
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { Container, Box, Text } from "./ui"
-import Feature from "./feature"
-import { IGatsbyImageData } from "gatsby-plugin-image"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Container, Box, Text } from "./ui";
+import Feature from "./feature";
+import { IGatsbyImageData } from "gatsby-plugin-image";
 
 export interface FeatureListData {
-  kicker: string,
-  heading: string,
-  features: FeaturesData[]
+  kicker: string;
+  heading: string;
+  features: FeaturesData[];
 }
 
 export interface FeaturesData {
-  id: string,
-  image: IGatsbyImageData,
-  kicker: string,
-  heading: string,
-  text: string
+  id: string;
+  image: IGatsbyImageData;
+  kicker: string;
+  heading: string;
+  text: string;
 }
 
-
 export default function FeatureList() {
-
   const queryData = useStaticQuery(graphql`
-  query {
-    dataJson {
-      featureList {
-        heading
-        kicker
-        features {
+    query {
+      dataJson {
+        featureList {
           heading
-          id
-          imageName
           kicker
-          text
+          features {
+            heading
+            id
+            imageName
+            kicker
+            text
+          }
         }
       }
-    }
-    allFile(filter: {relativeDirectory: {eq: "features"}}) {
-      nodes {
-        childImageSharp {
-          gatsbyImageData(placeholder: BLURRED)
-          fixed {
-            originalName
+      allFile(filter: { relativeDirectory: { eq: "features" } }) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+            fixed {
+              originalName
+            }
           }
         }
       }
     }
-  }
-`)
+  `);
 
   function mergeArr(data: any[], imgs: any[]): any[] {
-    return data.map(d => ({
-      ...imgs.find((i) => (i.childImageSharp.fixed.originalName === d.imageName)),
-      ...d
-    }))
+    return data.map((d) => ({
+      ...imgs.find((i) => i.childImageSharp.fixed.originalName === d.imageName),
+      ...d,
+    }));
   }
 
-  const mergeData = mergeArr(queryData.dataJson.featureList.features,
-    queryData.allFile.nodes)
-
+  const mergeData = mergeArr(
+    queryData.dataJson.featureList.features,
+    queryData.allFile.nodes
+  );
 
   const data: FeatureListData = {
     kicker: queryData.dataJson.featureList.kicker,
     heading: queryData.dataJson.featureList.heading,
-    features: mergeData.map(i => ({
+    features: mergeData.map((i) => ({
       id: i.id,
       heading: i.heading,
       kicker: i.kicker,
       text: i.text,
       image: i.childImageSharp.gatsbyImageData,
-    }))
-  }
+    })),
+  };
 
   return (
     <Container width="fullbleed">
       <Box background="muted" radius="large">
-        <Box center paddingY={5}>
+        <Box center>
           <Text variant="kicker">{data.kicker}</Text>
-          <Text as="h2" variant="heading">{data.heading}</Text>
+          <Text as="h2" variant="heading">
+            {data.heading}
+          </Text>
         </Box>
         {data.features.map((feature, i) => (
           <Feature key={feature.id} {...feature} flip={Boolean(i % 2)} />
         ))}
       </Box>
     </Container>
-  )
+  );
 }
